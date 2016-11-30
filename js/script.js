6e005c35aeb035ae24438005368232d05b813cdc
Le Viet Hoc <Le Viet Hoc>
2016-11-30 09:54:50 +0700

    help

--- js/script.js
+++ js/script.js
@@ -35,3 +35,22 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
   }
 }
+
+$scope.slider = {
+  minValue: 100,
+  maxValue: 400,
+  options: {
+    floor: 0,
+    ceil: 500,
+    translate: function(value, sliderId, label) {
+      switch (label) {
+        case 'model':
+          return '<b>Min price:</b> $' + value;
+        case 'high':
+          return '<b>Max price:</b> $' + value;
+        default:
+          return '$' + value
+      }
+    }
+  }
+};
